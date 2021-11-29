d213a2e4794abbcad467aa4402656ed63adbe422
Belle Duran <bduran04@gmail.com>
2021-11-28 18:32:43 -0600

    added header & material

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography'
+
+export default function Header() {
+  return (
+    <Typography variant="h2" gutterBottom>
+      Car Dealer App
+    </Typography>
+  );
+}
